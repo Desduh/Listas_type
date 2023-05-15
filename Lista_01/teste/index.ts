07470705a1a07cdddd57030cad1753fcdb97ff72
Desduh <eduardofalandess@gmail.com>
2023-05-15 16:33:39 -0300

    Adição da opção de poder adicionar telefones

--- Lista_01/teste/index.ts
+++ Lista_01/teste/index.ts
@@ -1,5 +1,6 @@
 import Cliente from "../modelos/cliente";
 import Endereco from "../modelos/endereco";
+import Telefone from "../modelos/telefone";
 let cliente = new Cliente()
 cliente.nome = `Pedro de Alcântara João Carlos Leopoldo Salvador`
 cliente.nomeSocial = `Dom Pedro II`
@@ -14,12 +15,21 @@ endereco.pais = `Brasil`
 endereco.codigoPostal = `22220-000`
 cliente.endereco = endereco
 
+let telefone = new Telefone()
+telefone.ddd = "21"
+telefone.numero = "988372758"
+cliente.telefones.push(telefone)
+telefone.ddd = "21"
+telefone.numero = "988303089"
+cliente.telefones.push(telefone)
+
 let dependente = new Cliente()
 dependente.nome = `Isabel Cristina Leopoldina Augusta Micaela`
 dependente.nomeSocial = `Princesa Isabel`
 dependente.dataCadastro = new Date(1921, 10, 14)
 dependente.dataNascimento = new Date(1846, 6, 29)
 dependente.endereco = (cliente.endereco.clonar() as Endereco)
+dependente.telefones = ( cliente.telefones)
 dependente.titular = cliente
 cliente.dependentes.push(dependente)
 
