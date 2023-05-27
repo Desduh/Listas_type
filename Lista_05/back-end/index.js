747ca89883900dbbcda8acad66106be00997a28f
Desduh <eduardofalandess@gmail.com>
2023-05-27 18:34:26 -0300

    Adição de documentos e telefones a um usuário

--- Lista_05/back-end/index.js
+++ Lista_05/back-end/index.js
@@ -12,6 +12,26 @@ async function criarTabelaClientes() {
   return client.execute(query);
 }
 
+async function criarTabelaTelefones() {
+  const query = "CREATE TABLE IF NOT EXISTS atlantis.telefones (id uuid PRIMARY KEY, ddd text, numero text);";
+  return client.execute(query);
+}
+
+async function criarTabelaClienteTelefone() {
+  const query = "CREATE TABLE IF NOT EXISTS atlantis.cliente_telefone (id uuid PRIMARY KEY, id_cliente uuid, id_telefone uuid);";
+  return client.execute(query);
+}
+
+async function criarIndiceIdClienteTelefone() {
+  const query = "CREATE INDEX IF NOT EXISTS id_cliente_cli_tell ON atlantis.cliente_telefone (id_cliente);";
+  return client.execute(query);
+}
+
+async function criarIndiceClienteIdTelefone() {
+  const query = "CREATE INDEX IF NOT EXISTS id_tell_cli_tell ON atlantis.cliente_telefone (id_telefone);";
+  return client.execute(query);
+}
+
 async function criarTabelaRgs() {
   const query = "CREATE TABLE IF NOT EXISTS atlantis.rgs (id uuid PRIMARY KEY, numero text, emissao date);";
   return client.execute(query);
@@ -31,10 +51,10 @@ async function criarIndiceClienteIdRg() {
   const query = "CREATE INDEX IF NOT EXISTS id_rg_cli_rg ON atlantis.cliente_rg (id_rg);";
   return client.execute(query);
 }
-
 const id = uuidv4();
+async function inserirUsuario(nome, nomeSocial, nascimento, cpf, passaporte, rgs, telefones) {
+  // const id = uuidv4();
 
-async function inserirUsuario(nome, nomeSocial, nascimento, cpf, passaporte, rgs) {
   const queryInserirCliente = 'INSERT INTO atlantis.clientes (id, nome, nome_social, nascimento, cpf, passaporte) VALUES (?, ?, ?, ?, ?, ?)';
   const parametrosCliente = [id, nome, nomeSocial, nascimento, cpf, passaporte];
   await client.execute(queryInserirCliente, parametrosCliente, { prepare: true });
@@ -48,6 +68,17 @@ async function inserirUsuario(nome, nomeSocial, nascimento, cpf, passaporte, rgs
     const queryInserirClienteRg = 'INSERT INTO atlantis.cliente_rg (id, id_cliente, id_rg) VALUES (?, ?, ?)';
     const parametrosClienteRg = [uuidv4(), id, rgId];
     await client.execute(queryInserirClienteRg, parametrosClienteRg, { prepare: true });
+  }
+
+  for (const telefone of telefones) {
+    const telefoneId = uuidv4();
+    const queryInserirTelefone = 'INSERT INTO atlantis.telefones (id, ddd, numero) VALUES (?, ?, ?)';
+    const parametrosTelefone = [telefoneId, telefone.ddd, telefone.numero];
+    await client.execute(queryInserirTelefone, parametrosTelefone, { prepare: true });
+
+    const queryInserirClienteTelefone = 'INSERT INTO atlantis.cliente_telefone (id, id_cliente, id_telefone) VALUES (?, ?, ?)';
+    const parametrosClienteTelefone = [uuidv4(), id, telefoneId];
+    await client.execute(queryInserirClienteTelefone, parametrosClienteTelefone, { prepare: true });
   }
 }
 
@@ -80,7 +111,36 @@ async function selecionarClienteRg(id) {
   return resultado;
 }
 
-async function selecionarUsuario(id) {
+async function selecionarTelefone(id) {
+  const querySelecionarTelefone = `
+  SELECT
+    ddd,
+    numero
+  FROM
+    atlantis.telefones
+  WHERE
+    id = ?
+  `;
+  const parametros = [id];
+  const resultado = await client.execute(querySelecionarTelefone, parametros, { prepare: true });
+  return resultado;
+}
+
+async function selecionarClienteTelefone(id) {
+  const querySelecionarClienteTelefone = `
+  SELECT
+    id_telefone
+  FROM
+    atlantis.cliente_telefone
+  WHERE
+    id_cliente = ?
+  `;
+  const parametros = [id];
+  const resultado = await client.execute(querySelecionarClienteTelefone, parametros, { prepare: true });
+  return resultado;
+}
+
+async function selectCliente(id) {
   const querySelecionarCliente = `
   SELECT
     nome,
@@ -98,36 +158,19 @@ async function selecionarUsuario(id) {
   return resultado;
 }
 
-async function atualizarUsuario(id, idade) {
-  const queryAtualizar = 'UPDATE atlantis.clientes SET idade = ? WHERE id = ?';
-  return client.execute(queryAtualizar, [idade, id], { prepare: true });
-}
-
-async function deletarUsuario(id) {
-  const queryRemover = 'DELETE FROM atlantis.clientes WHERE id = ?';
-  const parametros = [id];
-  return client.execute(queryRemover, parametros, { prepare: true });
-}
-
-async function exemplo() {
-  await client.connect();
-  await criarTabelaClientes();
-  await criarTabelaRgs();
-  await criarTabelaClienteRg();
-  await criarIndiceIdClienteRg();
-  await criarIndiceClienteIdRg();
-
-  const rgs = [
-    { numero: 'RG123', emissao: '2022-01-01' },
-    { numero: 'RG456', emissao: '2023-02-02' }
-  ];
-
-  await inserirUsuario('Jones', 'Bob', '1990-01-01', '123456789', 'ABCD1234', rgs);
-
-  const resultadoUsuario = await selecionarUsuario(id);
+// funções para o front
+async function clienteCompleto(id) {
+  var usuario = {}
+  const resultadoUsuario = await selectCliente(id);
   if (resultadoUsuario && resultadoUsuario.first()) {
-    const usuario = resultadoUsuario.first();
-    console.log("Nome: %s, Nome Social: %s, Nascimento: %s, CPF: %s, Passaporte: %s", usuario.nome, usuario.nome_social, usuario.nascimento, usuario.cpf, usuario.passaporte);
+    const usuarioOld = resultadoUsuario.first();
+    usuario = {
+      nome: usuarioOld.nome, 
+      nome_social: usuarioOld.nome_social, 
+      nascimento: usuarioOld.nascimento.toString(), 
+      cpf: usuarioOld.cpf, 
+      passaporte: usuarioOld.passaporte
+    }
     const resultadoIdRgs = await selecionarClienteRg(id);
     if (resultadoIdRgs && resultadoIdRgs.rowLength > 0) {
       const rgsArray = [];
@@ -136,17 +179,59 @@ async function exemplo() {
         const resultadoRg = await selecionarRg(idRg);
         rgsArray.push(resultadoRg.first());
       }
-      console.log("RGs:");
-      console.log(rgsArray.map(rg => ({ numero: rg.numero, emissao: rg.emissao.date })));
-    } else {
-      console.log("Nenhum resultado encontrado");
+      usuario.rgs = rgsArray.map(rg => ({ numero: rg.numero, emissao: rg.emissao.toString()  }))
+    } 
+
+    const resultadoIdTelefones = await selecionarClienteTelefone(id);
+    if (resultadoIdTelefones && resultadoIdTelefones.rowLength > 0) {
+      const telefonesArray = [];
+      for (const row of resultadoIdTelefones.rows) {
+        const idTelefone = row.id_telefone;
+        const resultadoTelefone = await selecionarTelefone(idTelefone);
+        telefonesArray.push(resultadoTelefone.first());
+      }
+      usuario.telefones = telefonesArray.map(telefone => ({ ddd: telefone.ddd, numero: telefone.numero }))
     }
-  } else {
-    console.log("Nenhum resultado encontrado");
   }
+  return usuario
+}
+
+
+async function exemplo() {
+  await client.connect();
+  await criarTabelaClientes();
+  await criarTabelaTelefones();
+  await criarTabelaClienteTelefone();
+  await criarIndiceIdClienteTelefone();
+  await criarIndiceClienteIdTelefone();
+  await criarTabelaRgs();
+  await criarTabelaClienteRg();
+  await criarIndiceIdClienteRg();
+  await criarIndiceClienteIdRg();
+
+  // Exemplo de inserção de um usuário
+  const usuario = {
+    nome: 'John Doe',
+    nomeSocial: 'Jane Doe',
+    nascimento: '1990-01-01',
+    cpf: '123456789',
+    passaporte: 'ABC123',
+    rgs: [
+      { numero: '987654321', emissao: '2020-01-01' },
+      { numero: '567890123', emissao: '2021-01-01' }
+    ],
+    telefones: [
+      { ddd: '11', numero: '999999999' },
+      { ddd: '22', numero: '888888888' }
+    ]
+  };
+
+  await inserirUsuario(usuario.nome, usuario.nomeSocial, usuario.nascimento, usuario.cpf, usuario.passaporte, usuario.rgs, usuario.telefones);
+
+
+  console.log(await clienteCompleto(id));
 
   await client.shutdown();
 }
 
 exemplo();
-
