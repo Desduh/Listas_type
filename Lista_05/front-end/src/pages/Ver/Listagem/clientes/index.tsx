e8d64f84dced55640bdd3020064d73485ea363c6
Desduh <eduardofalandess@gmail.com>
2023-05-27 21:16:24 -0300

    Ajustes de perfomace

--- Lista_05/front-end/src/pages/Ver/Listagem/clientes/index.tsx
+++ Lista_05/front-end/src/pages/Ver/Listagem/clientes/index.tsx
@@ -5,69 +5,99 @@ import NavBar_ from '../../../../component/barraNavegacao';
 import '../../Listagem/styles.css';
 
 interface Cliente {
-    id: string;
-    nome: string;
-    cpf: string;
-  }
-  
+  id: string;
+  nome: string;
+  cpf: string;
+}
+
 function Clientes() {
-    const [clientes, setClientes] = useState<Cliente[]>([]);
+  const [clientes, setClientes] = useState<Cliente[]>([]);
 
-    useEffect(() => {
-        axios.get('http://localhost:3001/clientes')
-            .then(response => {
-                setClientes(response.data);
-            })
-            .catch(error => {
-                console.log(error);
-            });
-    }, []);
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/clientes')
+      .then(response => {
+        setClientes(response.data);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }, []);
 
-    console.log(clientes);    
+  function removerCliente(id: string) {
+    axios
+      .delete(`http://localhost:3001/deletar/cliente`, { data: { id } })
+      .then(() => {
+            axios
+        .get('http://localhost:3001/clientes')
+        .then(response => {
+            setClientes(response.data);
+        })
+        .catch(error => {
+            console.log(error);
+        });
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
 
-    return (
-        <section>
-            <header>
-                <NavBar_ />
-            </header>
-            <main>
-                <div className='text'>
-                    <h1 className="titles"> <strong> Clientes WB </strong> </h1>
-                </div>
-                <div className="tables">
-                {clientes.length > 0 ? (
-                    <Table striped bordered hover variant="light">
-                        <thead className="titles-table">
-                            <tr>
-                                <th>Nome</th>
-                                <th>CPF</th>
-                                <th>Ver</th>
-                                <th>Editar</th>
-                                <th>Excluir</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            
-                                {clientes.map(cliente => (
-                                    <tr key={cliente.id}>
-                                        <td>{cliente.nome}</td>
-                                        <td>{cliente.cpf}</td>
-                                        <td><a className="editar-cps" href={`/cliente/${cliente.id}`}>Ver</a></td>
-                                        <td><a className="editar-cps" href={`/editar_cliente/${cliente.id}`}>Editar</a></td>
-                                        <td><a className="remover-cps" href="/" type='submit'>Remover</a></td>
-                                    </tr>
-                                ))}
-                        </tbody>
-                    </Table>
-                ) : (
-                    <tr>
-                        <td>Sem dados</td>
-                    </tr>
-                )}
-                </div>
-            </main>
-        </section>
-    );
+  return (
+    <section>
+      <header>
+        <NavBar_ />
+      </header>
+      <main>
+        <div className="text">
+          <h1 className="titles">
+            <strong>Clientes WB</strong>
+          </h1>
+        </div>
+        <div className="tables">
+          {clientes.length > 0 ? (
+            <Table striped bordered hover variant="light">
+              <thead className="titles-table">
+                <tr>
+                  <th>Nome</th>
+                  <th>CPF</th>
+                  <th>Ver</th>
+                  <th>Editar</th>
+                  <th>Excluir</th>
+                </tr>
+              </thead>
+              <tbody>
+                {clientes.map(cliente => (
+                  <tr key={cliente.id}>
+                    <td>{cliente.nome}</td>
+                    <td>{cliente.cpf}</td>
+                    <td>
+                      <a className="editar-cps" href={`/cliente/${cliente.id}`}>
+                        Ver
+                      </a>
+                    </td>
+                    <td>
+                      <a className="editar-cps" href={`/editar_cliente/${cliente.id}`}>
+                        Editar
+                      </a>
+                    </td>
+                    <td>
+                      <button className="remover-cps" onClick={() => removerCliente(cliente.id)}>
+                        Remover
+                      </button>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </Table>
+          ) : (
+            <tr>
+              <td>Sem dados</td>
+            </tr>
+          )}
+        </div>
+      </main>
+    </section>
+  );
 }
 
 export default Clientes;
